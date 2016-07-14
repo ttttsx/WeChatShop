dc02302f714d5f9cfe9140db3339c8b1c0988220
tsx <tsxmail@sina.com>
2016-07-14 19:49:11 +0800

    图片轮播和导航

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -1,7 +1,17 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
-	'token' => 'mytoken',
-	'appid' => 'wx2a52f4b034f31e2b',
-	'appsecret' => '6326123d60ebe34dfd78edb0b80841ef',
+	'TOKEN' => 'mytoken', // token值
+	'APPID' => 'wx2a52f4b034f31e2b', // 微信公众号的appid
+	'APPSECRET' => '6326123d60ebe34dfd78edb0b80841ef', // 微信公众号的appsecret
+		
+	// 数据库配置信息
+	'DB_TYPE' => 'mysql', // 数据库类型
+	'DB_HOST' => 'localhost', // 服务器地址
+	'DB_NAME' => '', // 数据库名
+	'DB_USER' => '', // 用户名
+	'DB_PWD' => '', // 密码
+	'DB_PORT' => '3306', // 端口
+	'DB_PREFIX' => '', // 数据库表前缀
+	'DB_CHARSET' => 'utf8' // 字符集
 );
